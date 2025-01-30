769f4ce50249a7782c5d8b183932ead0f5c455dc
Aditya Agarwal <tyadi3110@kgpian.iitkgp.ac.in>
2025-01-30 14:27:52 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import crypto from 'crypto';
 import path from "path";
 import {json} from "stream/consumers";
 
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 
 const DATA_FILE = path.join("data", "links.json");
 
